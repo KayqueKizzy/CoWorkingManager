61e1a3ed3c0df42fc17a946d802cf49ddfd5ed3a
KayqueKizzy <kayquesantos999@gmail.com>
2025-11-18 21:48:48 -0300

    Subindo a classe EspacoController para o pacote controller

--- src/main/java/kayquemarques/controller/EspacoController.java
+++ src/main/java/kayquemarques/controller/EspacoController.java
@@ -1,4 +1,51 @@
 package kayquemarques.controller;
 
+import kayquemarques.model.Espaco;
+import kayquemarques.model.SalaDeReuniao;
+import kayquemarques.model.CabineIndividual;
+import kayquemarques.model.Auditorio;
+import kayquemarques.service.EspacoService;
+
+import java.util.List;
+
 public class EspacoController {
+
+    private final EspacoService service;
+
+    public EspacoController(EspacoService service) {
+        this.service = service;
+    }
+
+    public void cadastrarSalaDeReuniao(int id, String nome, int capacidade, boolean disponivel,
+                                       double precoPorHora, boolean usaProjetor) {
+
+        Espaco espaco = new SalaDeReuniao(id, nome, capacidade, disponivel, precoPorHora, usaProjetor);
+        service.salvar(espaco);
+    }
+
+    public void cadastrarCabine(int id, String nome, int capacidade, boolean disponivel,
+                                double precoPorHora) {
+
+        Espaco espaco = new CabineIndividual(id, nome, capacidade, disponivel, precoPorHora);
+        service.salvar(espaco);
+    }
+
+    public void cadastrarAuditorio(int id, String nome, int capacidade, boolean disponivel,
+                                   double precoPorHora, boolean temPalco, int capacidadeExtra) {
+
+        Espaco espaco = new Auditorio(id, nome, capacidade, disponivel, precoPorHora, temPalco, capacidadeExtra);
+        service.salvar(espaco);
+    }
+
+    public List<Espaco> listar() {
+        return service.buscarTodos();
+    }
+
+    public Espaco buscarPorId(int id) {
+        return service.buscarPorId(id);
+    }
+
+    public void remover(int id) {
+        service.remover(id);
+    }
 }
